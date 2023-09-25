5d23d6630e67e6a34ee3bdc696738012050e841e
frodriguez0023 <frodriguez0023@CEP-AULA7-PC18.andalucia.fusp.ceu>
2023-09-25 20:57:21 +0200

    .

--- Swing/src/Ejercicio7_8_9/Ejercicio7_8_9.java
+++ Swing/src/Ejercicio7_8_9/Ejercicio7_8_9.java
@@ -1,5 +1,4 @@
-package Ejercicio7_8;
-
+package Ejercicio7_8_9;
 
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -13,7 +12,7 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JTextField;
 
-public class Ejercicio7_8 {
+public class Ejercicio7_8_9 {
 
 	private JFrame frame;
 	private JTextField txt1;
@@ -28,7 +27,7 @@ public class Ejercicio7_8 {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Ejercicio7_8 window = new Ejercicio7_8();
+					Ejercicio7_8_9 window = new Ejercicio7_8_9();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -40,7 +39,7 @@ public class Ejercicio7_8 {
 	/**
 	 * Create the application.
 	 */
-	public Ejercicio7_8() {
+	public Ejercicio7_8_9() {
 		initialize();
 	}
 
@@ -52,74 +51,71 @@ public class Ejercicio7_8 {
 		frame.setBounds(100, 100, 742, 562);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		txt1 = new JTextField();
 		txt1.setBounds(309, 45, 86, 20);
 		frame.getContentPane().add(txt1);
 		txt1.setColumns(10);
-		
+
 		txt2 = new JTextField();
 		txt2.setBounds(309, 114, 86, 20);
 		frame.getContentPane().add(txt2);
 		txt2.setColumns(10);
-		
+
 		txt3 = new JTextField();
 		txt3.setBounds(309, 199, 86, 20);
 		frame.getContentPane().add(txt3);
 		txt3.setColumns(10);
-		
+
 		txt4 = new JTextField();
 		txt4.setBounds(309, 278, 86, 20);
 		frame.getContentPane().add(txt4);
 		txt4.setColumns(10);
-		
-		
-		
+
 		KeyListener k1 = new KeyListener() {
-			
+
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
-				
+
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode()==40 || e.getKeyCode()==KeyEvent.VK_ENTER) {
+				if (e.getKeyCode() == 40 || e.getKeyCode() == KeyEvent.VK_ENTER) {
 					e.getComponent().transferFocus();
-				}else if (e.getKeyCode()==38) {
+				} else if (e.getKeyCode() == 38) {
 					e.getComponent().transferFocusBackward();
 				}
-				
+
 			}
 		};
-		
+
 		FocusListener fc = new FocusListener() {
-			
+
 			@Override
 			public void focusLost(FocusEvent e) {
 				e.getComponent().setBackground(null);
-				
+
 			}
-			
+
 			@Override
 			public void focusGained(FocusEvent e) {
 				e.getComponent().setBackground(Color.YELLOW);
-				
+
 			}
 		};
-		
+
 		txt1.addFocusListener(fc);
 		txt2.addFocusListener(fc);
 		txt3.addFocusListener(fc);
 		txt4.addFocusListener(fc);
-		
+
 		txt1.addKeyListener(k1);
 		txt2.addKeyListener(k1);
 		txt3.addKeyListener(k1);
